eafb4db0632e23ef2c6a0f01028c1a5a24a03b67
Herhel Horvat <herhel.horvat@teaminternational.com>
2022-10-02 14:40:45 +0300

    add useMediaQuery

--- src/pages/MuiBreakpoints/index.jsx
+++ src/pages/MuiBreakpoints/index.jsx
@@ -2,6 +2,7 @@ import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@mui/styles';
+import breakpoints from '../../images/breakpoints.png';
 
 const useStyles = makeStyles((theme) => ({
   page: { padding: 10 },
@@ -45,10 +46,10 @@ const MuiPage = () => {
   return (
     <div className={styles.page}>
       <Link
-        href="https://v4.mui.com/ru/customization/breakpoints/"
-        target="_blank"
-        rel="noreferrer"
-        underline="none"
+        href='https://v4.mui.com/ru/customization/breakpoints/'
+        target='_blank'
+        rel='noreferrer'
+        underline='none'
       >
         <h2>Breakpoints</h2>
       </Link>
@@ -57,17 +58,30 @@ const MuiPage = () => {
         <strong>Breakpoints:</strong> API, що дозволяє використовувати
         контрольні точки у широкому діапазоні контекстів.
       </p>
+      <img className={styles.img} src={breakpoints} alt='breakpoints' />
+
       <ul>
-        <li>theme.breakpoints.up(key)</li>
-        <li>theme.breakpoints.down(key)</li>
-        <li>theme.breakpoints.only(key)</li>
-        <li>theme.breakpoints.not(key)</li>
-        <li>theme.breakpoints.between(start, end)</li>
+        <li>
+          theme.breakpoints.<strong>up</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>down</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>only</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>not</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>between</strong>(start, end)
+        </li>
       </ul>
       <hr />
 
       <Container>
         <Paper elevation={3} className={styles.paper}>
+          <span className={styles.box}>up ►</span>
           <span className={[styles.box, styles.xs].join(' ')}>xs - 0</span>
           <span className={[styles.box, styles.sm].join(' ')}>sm - 600</span>
           <span className={[styles.box, styles.md].join(' ')}>md - 900</span>
--- src/pages/MuiUseMediaQuery/index.jsx
+++ src/pages/MuiUseMediaQuery/index.jsx
@@ -1,13 +1,103 @@
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+import breakpoints from '../../images/breakpoints.png';
 import styles from './styles.module.css';
 
-const MuiPage = () => (
-  <div className={styles.page}>
-    <h2>MUI Page</h2>
-    <p>
-      <strong>useMediaQuery:</strong> Це хук медіа-запиту для React. Він
-      відстежує збіги з медіа-запитом CSS.
-    </p>
-  </div>
+const TestContainer = ({ children }) => (
+  <>
+    <Container>
+      <Paper elevation={3} className={styles.paper}>
+        {children}
+      </Paper>
+    </Container>
+    <hr />
+  </>
 );
+
+const MuiPage = () => {
+  const theme = useTheme();
+  const matchByWidth = useMediaQuery('(max-width: 600px)'); // xs
+  const matchByUpSm = useMediaQuery(theme.breakpoints.up('sm'));
+  const matchByUpMd = useMediaQuery(theme.breakpoints.up('md'));
+  const matchByDownLg = useMediaQuery(theme.breakpoints.down('lg'));
+
+  return (
+    <div className={styles.page}>
+      <Link
+        href='https://v4.mui.com/ru/customization/breakpoints/'
+        target='_blank'
+        rel='noreferrer'
+        underline='none'
+      >
+        <h2>useMediaQuery</h2>
+      </Link>
+
+      <p>
+        <strong>useMediaQuery:</strong> Це хук медіа-запиту для React. Він
+        відстежує збіги з медіа-запитом CSS.
+      </p>
+      <img className={styles.img} src={breakpoints} alt='breakpoints' />
+      <hr />
+
+      <ul>
+        <li>
+          theme.breakpoints.<strong>up</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>down</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>only</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>not</strong>(key)
+        </li>
+        <li>
+          theme.breakpoints.<strong>between</strong>(start, end)
+        </li>
+      </ul>
+      <hr />
+
+      {matchByWidth && (
+        <TestContainer>
+          <p>
+            useMediaQuery('<strong>max-width: 600px</strong>')
+          </p>
+        </TestContainer>
+      )}
+
+      {matchByUpSm && (
+        <TestContainer>
+          <p>
+            <strong>useMediaQuery</strong>(theme.breakpoints.up('
+            <strong>sm</strong>'))
+          </p>
+        </TestContainer>
+      )}
+
+      {matchByUpMd && (
+        <TestContainer>
+          <p>
+            <strong>useMediaQuery</strong>(theme.breakpoints.up('
+            <strong>md</strong>'))
+          </p>
+        </TestContainer>
+      )}
+
+      {matchByDownLg && (
+        <TestContainer>
+          <p>
+            <strong>useMediaQuery</strong>(theme.breakpoints.down('
+            <strong>lg</strong>'))
+          </p>
+        </TestContainer>
+      )}
+    </div>
+  );
+};
 
 export default MuiPage;
